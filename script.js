337559e85c860b604af776689d4a4ebb407f2294
Welder Lopes <welderlsantiago@gmail.com>
2019-04-19 13:24:59 -0300

    Ajustar navbar e responsividade

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ let menuHam = document.querySelector(".eixoCont");
 let menuHam2 = document.querySelector(".eixoSome");
 let menuHam1 = document.querySelector(".eixoDir");
 let acaoMenu = document.querySelector("div");
-let navbarMenu= document.querySelector("nav")
+let navbarMenu= document.querySelector("nav");
+
 function menuX(){
     menuHam.classList.toggle("movimento");
     menuHam1.classList.toggle("movimentoDois");

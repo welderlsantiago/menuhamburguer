74c1d630d6f5d90d82060ae15bea48f23843829f
Welder Lopes <welderlsantiago@gmail.com>
2019-04-19 12:55:07 -0300

    Criar navbar responsiva com menu hamburguer e x com barras

---
+++ script.js
@@ -0,0 +1,13 @@
+let menuHam = document.querySelector(".eixoCont");
+let menuHam2 = document.querySelector(".eixoSome");
+let menuHam1 = document.querySelector(".eixoDir");
+let acaoMenu = document.querySelector("div");
+let navbarMenu= document.querySelector("nav")
+function menuX(){
+    menuHam.classList.toggle("movimento");
+    menuHam1.classList.toggle("movimentoDois");
+    menuHam2.classList.toggle("some");
+    navbarMenu.classList.toggle("movContent");
+}
+
+acaoMenu.onclick= menuX;
